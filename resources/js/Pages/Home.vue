6c244cda01c7487ed08d6ea49b9e0bb4be98f60b
João Paulo <joaopauloribeiropontes@gmail.com>
2023-06-07 14:14:23 -0300

    Alteração de preço

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -134,14 +134,14 @@ onMounted(() => {
                     <div class="col-12 col-md-4 mb-4">
                         <Plan
                             name="Pinguço"
-                            :amount="160"
+                            :amount="180"
                             :includedIn="[
                                 'Sítio',
                                 'Comida',
                                 'Churrasco',
                                 'Chope',
                             ]"
-                            @buy="openBuyModal(160)"
+                            @buy="openBuyModal(180)"
                         />
                     </div>
                     <div class="col-12 col-md-4 mb-4">
